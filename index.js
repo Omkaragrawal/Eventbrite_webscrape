0b8b9e75a670d49c1c3c9672fe75328603dda249
OMKAR AGRAWAL <Omkaragrawal@users.noreply.github.com>
2019-12-02 20:21:56 +0530

    done dana done done

--- index.js
+++ index.js
@@ -23,26 +23,30 @@ const data = [[
     "Shaikh"
   ]];
 
+const registrarEmail = '';
+const registrarPassword = '';
+
 app.use(morgan('combined'));
 app.listen(8080, () => {
     console.log('hosted on 8080');
 });
 (async () => {
     const browser = await puppeteer.launch({
-        headless: false
+        headless: true
     });
     const page = await browser.newPage();
     await page.goto('https://www.eventbrite.com/attendees-add?eid=84591759229', {
-        waitUntil: 'networkidle0'
+        waitUntil: 'networkidle0',
+        timeout:0
     });
 
     if (page.url().includes('signin')) {
-        await page.type('#email', 'agrawalomkar.16@gmail.com');
+        await page.type('#email', registrarEmail);
         await page.click('[data-reactid *= "125"]');
-        await page.waitForNavigation({waitUntil: 'networkidle0'});
-        await page.type('#password', 'abcd@1234');
+        await page.waitForNavigation({waitUntil: 'networkidle0', timeout:0});
+        await page.type('#password', registrarPassword);
         await page.click('[data-automation *="signup-submit"]');
-        await page.waitForNavigation({ waitUntil: 'networkidle0' });
+        await page.waitForNavigation({ waitUntil: 'networkidle0', timeout: 0 });
         register(page);
     } else {
         register(page);
@@ -58,11 +62,14 @@ const register = async (page) => {
 const fillForm = async (page, details) => {
     await page.type('[maxLength *= "5"]', '1');
     await page.click('#continue-attendee');
-    await page.waitForNavigation({ waitUntil: 'networkidle2' });
+    // await page.waitForNavigation({ waitUntil: 'load', timeout: 0 });
+    await page.waitForSelector('#last_name');
     await page.type('#last_name', details[2]);
     await page.type('#first_name', details[1]);
     await page.type('#email_address', details[0]);
+    await page.waitFor(3000);
+    await page.waitForSelector('#last_name');
     await page.click('[href *= "freeCheckout();"]');
-    await page.waitForNavigation({ waitUntil: 'networkidle0' });
-    await page.goto('https://www.eventbrite.com/attendees-add?eid=84591759229', {waitUntil: 'networkidle0'});
+    await page.waitForNavigation({ waitUntil: 'networkidle0', timeout:0 });
+    await page.goto('https://www.eventbrite.com/attendees-add?eid=84591759229', {waitUntil: 'load', timeout: 0});
 };
